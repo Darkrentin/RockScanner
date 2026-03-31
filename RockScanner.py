195fa8d47aec83142290eaeff689d9f9b2ea6ed1
Darkrentin <darkrentin@gmail.com>
2026-03-31 20:20:06 +0200

    Fix mining data

--- RockScanner.py
+++ RockScanner.py
@@ -20,13 +20,35 @@ pytesseract.pytesseract.tesseract_cmd = os.path.join(TESS_FOLDER, "tesseract.exe
 LOGO_PATH = resource_path("logo.png")
 
 MINING_DATA = {
-    3170: "Quantainium", 3185: "Stileron", 3200: "Savrilium", 3370: "Ouratite",
-    3385: "Riccite", 3400: "Lindinium", 3555: "Taranite",
-    3570: "Borase", 3585: "Gold", 3600: "Bexalite", 3825: "Laranite",
-    3840: "Aslarite", 3855: "Titanium", 3870: "Tungsten", 3885: "Agricium",
-    3900: "Torite", 4180: "Hephestanite", 4195: "Tin", 4210: "Quartz",
-    4225: "Corundum", 4240: "Copper", 4255: "Silicon", 4270: "Iron",
-    4285: "Aluminium", 4300: "Ice", 2000: "Ship wrecks"
+    3170: "Quantainium", 
+    3185: "Stileron",
+    3200: "Savrilium", 
+    3370: "Ouratite",
+    3385: "Riccite", 
+    3400: "Lindinium",
+    3540: "Beryl",
+    3555: "Taranite",
+    3570: "Borase",
+    3585: "Gold", 
+    3600: "Bexalite", 
+    3825: "Laranite",
+    3840: "Aslarite", 
+    3855: "Titanium", 
+    3870: "Tungsten", 
+    3885: "Agricium",
+    3900: "Torite", 
+    4180: "Hephestanite", 
+    4195: "Tin", 
+    4210: "Quartz",
+    4225: "Corundum",
+    4240: "Copper", 
+    4255: "Silicon", 
+    4270: "Iron",
+    4285: "Aluminium", 
+    4300: "Ice", 
+    2000: "Ship wrecks",
+    3000: "FPS Mineables",
+    4000: "Ground Vehicule Deposits"
 }
 
 class Style:
